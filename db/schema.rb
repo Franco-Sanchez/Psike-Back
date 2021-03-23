2fd492040e63d5ee43c19ca99742b7b0a0ba9a31
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-23 17:49:54 -0500

    Created Day model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_222411) do
+ActiveRecord::Schema.define(version: 2021_03_23_224711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_03_23_222411) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "days", force: :cascade do |t|
+    t.string "day_name"
+    t.integer "day_number"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "diagnoses", force: :cascade do |t|
