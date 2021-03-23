1ce8be5880259fe0b0d87ef7521e453539bce4ab
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-23 17:56:45 -0500

    Add default value to the status field on Diagnoses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_224711) do
+ActiveRecord::Schema.define(version: 2021_03_23_225330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_224711) do
   create_table "diagnoses", force: :cascade do |t|
     t.string "title"
     t.text "diagnosis"
-    t.boolean "status"
+    t.boolean "status", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "patient_id", null: false
