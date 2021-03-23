f2e47027f319c11e61c4e184367858996131f01e
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-23 13:52:24 -0500

    Validate format email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
   has_secure_token
 
   enum role: { admin: 0, patient: 1, psychologist: 2 }
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A\w+@gmail\.com\z/,
+                              message: 'Your email must similar to example@gmail.com' }
+  validates :password, length: { minimum: 6 }, allow_nil: true
 end
