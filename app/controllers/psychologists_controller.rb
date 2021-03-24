92b6ec9d84bed49a5a7a45da5bdca8d33c00069a
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-24 08:19:28 -0500

    Psychologists controller created whit routes to index and show

---
+++ app/controllers/psychologists_controller.rb
@@ -0,0 +1,5 @@
+class PsychologistsController < ApplicationController
+  def index; end
+
+  def show; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'health#index'
+
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
   post '/signup', to: 'users#create'
+
+  resources :psychologists, only: %i[index show]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
