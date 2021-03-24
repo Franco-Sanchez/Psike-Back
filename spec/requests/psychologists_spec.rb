0081dca822601778814a5ea8f8fb869460537561
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-24 08:34:58 -0500

    Appointments controller created

--- spec/requests/psychologists_spec.rb
+++ spec/requests/psychologists_spec.rb
@@ -1,18 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Psychologists", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/psychologists/index"
+RSpec.describe 'Psychologists', type: :request do
+  describe 'GET /index' do
+    it 'returns http success' do
+      get '/psychologists/index'
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/psychologists/show"
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/psychologists/show'
       expect(response).to have_http_status(:success)
     end
   end
-
 end
