6028c9db264829affce23de0db47bef777c4bff5
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-24 19:19:06 -0500

    Create action was added in Appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,36 @@
 class AppointmentsController < ApplicationController
+  before_action :found_patient
+
+  # GET /appointments
   def index
-    patient = Patient.find_by(user: current_user)
     psychologist = Psychologist.find_by(user: current_user)
-    if patient
-      render json: patient.appointments
+    if @patient
+      render json: @patient.appointments
     elsif psychologist
       render json: psychologist.appointments
     end
   end
 
-  def create; end
+  # POST /appointments
+  def create
+    diagnosis = @patient.diagnoses.where(status: false)
+    appointment = Appointment.new(appointment_params)
+    appointment.diagnosis = diagnosis
+    appointment.patient = @patient
+    if appointment.save
+      render json: appointment, status: :created
+    else
+      render json: appointment.errors, status: :bad_request
+    end
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:feedback, :status, :day, :reason, :psychologist, :schedule)
+  end
+
+  def found_patient
+    @patient = Patient.find_by(user: current_user)
+  end
 end
