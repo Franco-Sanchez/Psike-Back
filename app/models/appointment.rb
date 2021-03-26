1d2f2a46ef58c99a8e8993b880490534a4291637
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-03-26 18:44:13 -0500

    Updated relationship between Appointment and Transfer

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,8 +3,8 @@ class Appointment < ApplicationRecord
   belongs_to :patient
   belongs_to :psychologist
   belongs_to :diagnosis
+  belongs_to :transfer
   has_one :ranking, dependent: :destroy
-  has_one :transfer, dependent: :destroy
   has_many :comments, dependent: :destroy
 
   enum status: { taken: 0, canceled: 1, completed: 2 }
--- db/migrate/20210324032824_create_transfers.rb
+++ db/migrate/20210324032824_create_transfers.rb
@@ -4,7 +4,6 @@ class CreateTransfers < ActiveRecord::Migration[6.0]
       t.date :day
       t.integer :amount
       t.string :code
-      t.references :appointment, null: false, foreign_key: true
 
       t.timestamps
     end
