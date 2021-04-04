fbe6c6177b1fa374a8e256e422662794c14dbf72
Franco Sanchez Rodriguez <francorsr98@gmail.com>
2021-04-04 00:31:41 -0500

    Add Url column to Appointments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_04_015125) do
+ActiveRecord::Schema.define(version: 2021_04_04_052951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_04_04_015125) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "transfer_id", null: false
+    t.string "url"
     t.index ["diagnosis_id"], name: "index_appointments_on_diagnosis_id"
     t.index ["patient_id"], name: "index_appointments_on_patient_id"
     t.index ["psychologist_id"], name: "index_appointments_on_psychologist_id"
